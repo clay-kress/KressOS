4b4151ffc6f7ec075dabb907b80ee078a681e7c5
Clay Kress <clay.e.kress@gmail.com>
2022-09-22 14:05:48 -0400

    Attempted to add license GPL

--- drivers/interrupt.h
+++ drivers/interrupt.h
@@ -1,3 +1,23 @@
+/***********************************************************************/
+/* Copyright (c) 2022 Clay Kress                                       */
+/*                                                                     */
+/* This file is part of KressOS.                                       */
+/* KressOS is free software: you can redistribute it and/or modify it  */
+/* under the terms of the GNU General Public License as published by   */
+/* the Free Software Foundation, either version 3 of the License, or   */
+/* (at your option) any later version.                                 */
+/*                                                                     */
+/* Foobar is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty         */
+/* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.             */
+/*                                                                     */
+/* See the GNU General Public License for more details.                */
+/*                                                                     */
+/* You should have received a copy of the GNU General Public License   */
+/* along with Foobar. If not, see <https://www.gnu.org/licenses/>.     */
+/*                                                                     */
+/***********************************************************************/
+
 /*** PIC Defs *********************************************************/
 #define PIC1Command 0x20
 #define PIC1Data 0x21
--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -1,3 +1,23 @@
+/***********************************************************************/
+/* Copyright (c) 2022 Clay Kress                                       */
+/*                                                                     */
+/* This file is part of KressOS.                                       */
+/* KressOS is free software: you can redistribute it and/or modify it  */
+/* under the terms of the GNU General Public License as published by   */
+/* the Free Software Foundation, either version 3 of the License, or   */
+/* (at your option) any later version.                                 */
+/*                                                                     */
+/* Foobar is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty         */
+/* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.             */
+/*                                                                     */
+/* See the GNU General Public License for more details.                */
+/*                                                                     */
+/* You should have received a copy of the GNU General Public License   */
+/* along with Foobar. If not, see <https://www.gnu.org/licenses/>.     */
+/*                                                                     */
+/***********************************************************************/
+
 void keyboardHandler(void);                                             // Called everytime the keyboard interrupt fires
 void sendKeyboardCommand(int command);                                  // Sends a command to the keyboard (port 0x60)
 void sendControllerCommand(int command);                                // Sends a command to the keyboard controller (port 0x64)
